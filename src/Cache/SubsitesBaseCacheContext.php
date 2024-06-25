ea778dad6a0452bf16be56df13c371e7194bf832
Rupert Jabelman <rupert@jabelman.org>
2024-06-25 15:58:36 +0100

    Added interface for SubsiteService. Wrote tests for cache contexts.

--- src/Cache/SubsitesBaseCacheContext.php
+++ src/Cache/SubsitesBaseCacheContext.php
@@ -5,6 +5,7 @@ namespace Drupal\localgov_subsites_extras\Cache;
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Cache\Context\CacheContextInterface;
 use Drupal\localgov_subsites_extras\Service\SubsiteService;
+use Drupal\localgov_subsites_extras\Service\SubsiteServiceInterface;
 
 /**
  * Base class for subsites cache contexts.
@@ -14,10 +15,10 @@ abstract class SubsitesBaseCacheContext implements CacheContextInterface {
   /**
    * Constructor.
    *
-   * @param \Drupal\localgov_subsites_extras\Service\SubsiteService $subsiteService
+   * @param \Drupal\localgov_subsites_extras\Service\SubsiteServiceInterface $subsiteService
    *   The localgov_subsites_extras.service service.
    */
-  public function __construct(protected SubsiteService $subsiteService) {
+  public function __construct(protected SubsiteServiceInterface $subsiteService) {
   }
 
   /**

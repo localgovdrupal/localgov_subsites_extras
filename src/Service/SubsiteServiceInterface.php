1ea1d0443af4da8fd31df00c3df272727dfb4a51
Adnan <adnan.muhammad@croydon.gov.uk>
2024-08-15 14:36:59 +0100

    Docs: Better PHPDoc comment.
    Documenting the optional parameter for SubsiteServiceInterface::getHomePage().

--- src/Service/SubsiteServiceInterface.php
+++ src/Service/SubsiteServiceInterface.php
@@ -14,6 +14,10 @@ interface SubsiteServiceInterface {
    *
    * This will only call ::findHomePage() once per request, so it's fine to call
    * from multiple preprocess functions without a performance penalty.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   (optional) Useful for determining the Subsite homepage for a given child
+   *   page.
    */
   public function getHomePage(?NodeInterface $node = NULL): ?NodeInterface;
 
